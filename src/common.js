831fac4da5e28c45d4e4392dc5e4eaafe14af00d
JILS <jil.shah@cimconlighting.com>
2020-07-19 19:06:23 +0530

    common.js file updated

--- src/common.js
+++ src/common.js
@@ -1,21 +1,37 @@
-export const userData=[
-{id:1,
-name:'jil',
-batch:'MSCIT',
-pqunty:1,},
+export const userData = [
+    {
+        id: 1,
+        name: 'jil',
+        batch: 'MSCIT',
+        pqunty: 1
+    },
 
-{id:2,
-name:'shauri',
-batch:'MBBS',
-pqunty:2,},
+    {
+        id: 2,
+        name: 'shauri',
+        batch: 'MBBS',
+        pqunty: 2
+    },
 
-{id:3,
-name:'kavi',
-batch:'MBA',
-pqunty:3,},
+    {
+        id: 3,
+        name: 'kavi',
+        batch: 'MBA',
+        pqunty: 3
+    },
 
-
+    {
+        id: 4,
+        name: 'jisha',
+        batch: 'BCA',
+        pqunty: 4
+    },
+    {
+        id: 5,
+        name: 'varsha',
+        batch: 'Bcom',
+        pqunty: 5
+    },
 
 
 ]
-	
